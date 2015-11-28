26f8e1c9dc783f49a1ffb85886b09801c82defcb
lykeven <wangyanzxyq@yahoo.com>
2015-11-28 11:48:17 +0800

    add simple hospital

---
+++ guahao/Controllers/HospitalController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Web.Mvc;
+using guahao.Models;
+
+namespace guahao.Controllers
+{
+    public class HospitalController : Controller
+    {
+        // GET: Hospital
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult HospitalList(string cityname)
+        {
+            guahaoEntities es = new guahaoEntities();
+            var db = new guahaoEntities();
+            //var hos = db.hospital.Where(o => o.city.Equals(1)).FirstOrDefault();
+            var hos = (from h in es.hospital
+                               where h.city==1
+                               select h.name).FirstOrDefault();
+
+            ViewBag.name = hos;         
+            return View();
+        }
+    }
+}
